21e8c525b59be48eb34f82a3a5458a190408f65a
afocus <175228533@qq.com>
2015-10-20 16:51:02 +0800

    add template function for math

--- render/templateFunc.go
+++ render/templateFunc.go
@@ -11,6 +11,7 @@ var teamplateFuncs template.FuncMap
 func init() {
 	teamplateFuncs = template.FuncMap{
 		"formatTime": Tpl_FormatTime,
+		"math":       Tpl_Math,
 	}
 }
 
@@ -24,4 +25,40 @@ func Tpl_FormatTime(t interface{}, layout string) string {
 	default:
 	}
 	return time.Unix(t64, 0).Format(layout)
+}
+
+func Tpl_Math(method string, a, b interface{}) float64 {
+	var num1 float64
+	var num2 float64
+	switch a.(type) {
+	case int64:
+		num1 = float64(a.(int64))
+		num2 = float64(b.(int64))
+	case float64:
+		num1 = a.(float64)
+		num2 = b.(float64)
+	case float32:
+		num1 = float64(a.(float32))
+		num2 = float64(b.(float32))
+	case int:
+		num1 = float64(a.(int))
+		num2 = float64(b.(int))
+	case uint:
+		num1 = float64(a.(uint))
+		num2 = float64(b.(uint))
+	default:
+		return 0
+	}
+	switch method {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		return num1 / num2
+	default:
+		return 0
+	}
 }
